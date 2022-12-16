f6bc81ae34ca2252b1d47c141366e26c20c8b20a
ses1234567890 <ses1234567890@hotmail.es>
2022-12-16 17:46:28 +0000

    Need to do extras

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,48 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [todoList, setTodoList] = useState([]);
+  return (
+    <div className="mt-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-4 mx-auto">
+            <input
+              type="text"
+              className="form-control text-center"
+              placeholder="Add ToDo"
+              onKeyUp={(e) => {
+                if (e.key == "Enter" && e.target.value.trim() != "") {
+                  setTodoList([...todoList, e.target.value]);
+                  e.target.value = "";
+                }
+              }}
+            />
+            {todoList.map((todo, index) => {
+              return (
+                <div key={index} className="row alert border">
+                  <p className="col-8">{todo}</p>
+                  <p className="offset-2 col-2" onClick={() => {
+                    setTodoList(todoList.filter((e, i) => i != index))
+
+                    // let newArray = [];
+                    // for(let x = 0; x<todoList.length; x++) {
+                    //   if(x != index) {
+                    //     newArray.push(todoList[x])
+                    //   }
+                    // }
+                    // setTodoList(newArray);
+                  }}>X</p>
+                </div>
+              );
+            })}
+            <div className="row p-3 border">{todoList.length > 0 ? `${todoList.length} todos` : "No hay tareas"}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
